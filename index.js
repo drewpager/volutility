37ce59b0dfe67e7e9602efc7391926ad58f7a604
Drew Page <drewpager@Drews-iMac.local>
2021-06-21 09:02:52 -0700

    working on pulling peak volumes

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 const moment = require("moment");
 const Alpaca = require("@alpacahq/alpaca-trade-api");
-const alpaca = new Alpaca();
+const alpaca = new Alpaca({
+  keyId: `${process.env.APCA_API_KEY_ID}`,
+  secretKey: `${process.env.APCA_API_SECRET_KEY}`,
+  paper: true,
+  usePolygon: false
+});
 
-//Get daily price data for AAPL over the last 7 days.
-let bars = alpaca.getBarsV2(
-  "AAPL",
-  {
-    start: moment().subtract(7, "days").format(),
-    end: moment().subtract(20, "minutes").format(),
-    timeframe: "1Day",
-  },
-  alpaca.configuration
-);
-const barset = [];
+// test API
 
-for await (let b of bars) {
-  barset.push(b);
+async function API() {
+  let ticker = 'AAPL'
+  let bars = await alpaca.getBarsV2(
+    ticker, 
+    {
+      start: moment().subtract(7, "days").format(),
+      end: moment().subtract(20, "minutes").format(),
+      timeframe: '1Day'
+    },
+    alpaca.configuration
+  )
+  const barset = [];
+
+  for await (let b of bars) {
+    barset.push(b);
+  }
+  
+  const week_open = barset[0].OpenPrice;
+  const week_close = barset.slice(-1)[0].ClosePrice;
+  const percent_change = ((week_close - week_open)/week_open) * 100
+  for (let i = 0; i < barset.length; i++) {
+    let peak_vol = [];
+    peak_vol.push(barset[i].Volume);
+    console.log(Math.max(peak_vol[0]));
+  }
+  // const vol_max = Math.max(peak_vol[0]);
+  // console.log(`${ticker}'s price changed by ${percent_change}% this week, volume peaked at ${vol_max}!`);
+
+
 }
 
-const week_open = barset[0].OpenPrice;
-const week_close = barset.slice(-1)[0].ClosePrice;
-const percent_change = ((week_close - week_open) / week_open) * 100;
-
-console.log(`AAPL moved ${percent_change}% over the last 7 days`);
+API();
